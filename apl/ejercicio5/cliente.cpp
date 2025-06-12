9a16f131f5c180c02571aa4f58b7e07c67ee7803
IvanAbaca <ivanfabrizioabaca@gmail.com>
2025-06-12 19:29:19 -0300

    header added

--- apl/ejercicio5/cliente.cpp
+++ apl/ejercicio5/cliente.cpp
@@ -1,3 +1,11 @@
+/*
+43262563 Abaca, Ivan
+43971511 Guardati, Francisco
+45542385 Palmieri, Marco
+41566741 Piegari, Lucila Quiroga
+43780360 Romero, Lucas Nicolas
+*/
+
 #include <iostream>
 #include <cstring>
 #include <unistd.h>
