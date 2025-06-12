f847397fd429e96d7dad64b073ccd0a03bf2d083
lucasnromero <89700157+lucasnromero@users.noreply.github.com>
2025-06-12 11:57:30 -0300

    Update cliente.cpp

--- apl/ejercicio4/cliente.cpp
+++ apl/ejercicio4/cliente.cpp
@@ -1,30 +1,25 @@
-// cliente.cpp
-
 #include <iostream>
 #include <cstring>
 #include <csignal>
 #include <ctime>
 #include <cerrno>
 #include <cstdlib>
-
-#include <fcntl.h>      // O_* constantes
-#include <sys/mman.h>   // shm_*
-#include <sys/stat.h>   // mode constants
-#include <unistd.h>     // close, getopt
-#include <semaphore.h>  // sem_*
+#include <fcntl.h>      
+#include <sys/mman.h>   
+#include <sys/stat.h>   
+#include <unistd.h>     
+#include <semaphore.h>  
 
 #include "ahorcado_ipc.h"
 
 using namespace std;
 
-// Para ignorar SIGINT (Ctrl-C)
 void sigint_handler(int signo) {
     (void)signo;
-    // no hace nada
 }
 
 int main(int argc, char* argv[]) {
-    // 1) Parseo de argumentos: -n <nickname>
+
     string nickname;
     int opt;
     while ((opt = getopt(argc, argv, "n:h")) != -1) {
@@ -46,14 +41,12 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    // 2) Ignoro SIGINT con SA_RESTART
     struct sigaction sa_int{};
     sa_int.sa_handler = sigint_handler;
     sigemptyset(&sa_int.sa_mask);
     sa_int.sa_flags   = SA_RESTART;
     sigaction(SIGINT, &sa_int, nullptr);
 
-    // 3) Abro semáforos que el Servidor creó:
     sem_t *sem_client_mutex = sem_open(SEM_CLIENT_MUTEX, 0);
     sem_t *sem_client_ready = sem_open(SEM_CLIENT_READY, 0);
     sem_t *sem_phrase_ready = sem_open(SEM_PHRASE_READY, 0);
@@ -68,13 +61,11 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    // 4) Intento “conectarme”:
     if (sem_wait(sem_client_mutex) < 0) {
         perror("sem_wait(SEM_CLIENT_MUTEX)");
         return 1;
     }
 
-    // 5) Abro memoria compartida:
     int shm_fd = shm_open(SHM_NAME, O_RDWR, 0);
     if (shm_fd < 0) {
         cerr << "Error al abrir memoria compartida: ¿Servidor corriendo?\n";
@@ -91,11 +82,9 @@ int main(int argc, char* argv[]) {
     }
     SharedData* shm_ptr = reinterpret_cast<SharedData*>(addr);
 
-    // 6) Escribo mi nickname en memoria compartida:
     memset(shm_ptr->nickname, 0, MAX_NICK_LEN);
     strncpy(shm_ptr->nickname, nickname.c_str(), MAX_NICK_LEN - 1);
 
-    // 7) Avisa al servidor que ya pusimos nickname:
     if (sem_post(sem_client_ready) < 0) {
         perror("sem_post(SEM_CLIENT_READY)");
         munmap(shm_ptr, sizeof(SharedData));
@@ -104,7 +93,6 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    // 8) Espero a que el servidor haga post(SEM_PHRASE_READY):
     if (sem_wait(sem_phrase_ready) < 0) {
         perror("sem_wait(SEM_PHRASE_READY)");
         munmap(shm_ptr, sizeof(SharedData));
@@ -113,7 +101,7 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    // 9) Inicio de juego
+    // Inicio del juego
     cout << "=== ¡Juego del Ahorcado! ===\n";
     cout << "Jugador: " << nickname << "\n";
     cout << "La frase a adivinar tiene "
@@ -121,7 +109,6 @@ int main(int argc, char* argv[]) {
     cout << "Frase: " << shm_ptr->masked << "\n";
     cout << "Intentos restantes: " << shm_ptr->attempts_left << "\n";
 
-    // 10) Bucle de interacción:
     while (true) {
         if (shm_ptr->game_state == 3) break;
 
@@ -167,7 +154,6 @@ int main(int argc, char* argv[]) {
         }
     }
 
-    // 11) Desconexión limpia:
     munmap(shm_ptr, sizeof(SharedData));
     close(shm_fd);
     sem_post(sem_client_mutex);
