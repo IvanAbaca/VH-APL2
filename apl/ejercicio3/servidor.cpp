17112e6a17794c3534ec65682bc37973ce6a8f8b
IvanAbaca <ivanfabrizioabaca@gmail.com>
2025-06-12 21:59:26 -0300

    adding headers

--- apl/ejercicio3/servidor.cpp
+++ apl/ejercicio3/servidor.cpp
@@ -1,3 +1,11 @@
+/*
+43262563 Abaca, Ivan
+43971511 Guardati, Francisco
+43780360 Romero, Lucas Nicolas
+45542385 Palmieri, Marco
+41566741 Piegari, Lucila Quiroga
+*/
+
 #include <iostream>
 #include <fstream>
 #include <sstream>
